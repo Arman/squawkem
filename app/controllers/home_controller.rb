073d8ee0ac59c7584b98973a56da71f11c79562a
Arman <aaerman@hotmail.com>
2011-12-15 14:51:13 -0800

    updated main search to do a like search for business name and description in addition to searching near location

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,12 +2,21 @@ class HomeController < ApplicationController
    
   def index
     @subtitle = "Home"
-    if params[:search_near].present? && params[:search].present? && params[:search_near].present?
-      @businesses = Business.near(params[:search], 50, :order => :distance)
-      @list_header = "There are " + ( @businesses.count.count > 0 ? @businesses.count.count.to_s : "no" ) + " businesses nearby."
+
+    if params[:search]
+      if !params[:search_for].blank? && !params[:search_near].blank?
+        @businesses = Business.where("name like ? OR description like ?","%#{params[:search_for]}%","%#{params[:search_for]}%").near(params[:search_near], 50, :order => :distance).includes(:reviews)
+      elsif !params[:search_for].blank?
+        @businesses = Business.where("name like ? OR description like ?","%#{params[:search_for]}%","%#{params[:search_for]}%").includes(:reviews)
+      elsif !params[:search_near].blank?
+        @businesses = Business.near(params[:search_near], 50, :order => :distance).includes(:reviews)
+      else 
+        @businesses = Business.all
+      end
+      @list_header = "Showing search results"
     else
       @businesses = Business.all
-      @list_header = "Showing all " + @businesses.count.to_s + "  businesses."
+      @list_header = "Showing all businesses"
     end
     @json = @businesses.to_gmaps4rails
   end
