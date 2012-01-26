758de9b75bcf4f56c34676eec35f8830c12e1cba
Arman <aaerman@hotmail.com>
2012-01-26 13:22:26 -0800

    user model extended, modified sign in form

---
+++ db/migrate/20120126173900_add_columns_to_user_model.rb
@@ -0,0 +1,9 @@
+class AddColumnsToUserModel < ActiveRecord::Migration
+  def change
+  	 add_column :users, :first_name, :string
+  	 add_column :users, :last_name, :string
+  	 add_column :users, :zip_code, :string
+  	 add_column :users, :gender, :boolean
+  	 add_column :users, :birthdate, :datetime
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120104223123) do
+ActiveRecord::Schema.define(:version => 20120126173900) do
 
   create_table "businesses", :force => true do |t|
     t.string   "name"
@@ -76,6 +76,11 @@ ActiveRecord::Schema.define(:version => 20120104223123) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "zip_code"
+    t.boolean  "gender"
+    t.datetime "birthdate"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
